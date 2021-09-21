678c234ab1170efa3129ea03637b39e6313ecec1
Wei <wzheng100@gmail.com>
2021-09-21 16:08:41 -0700

    Removing from End

--- Lists/DoublyLinkedLists/src/com/zheng/EmployeeDoublyLinkedList.java
+++ Lists/DoublyLinkedLists/src/com/zheng/EmployeeDoublyLinkedList.java
@@ -20,6 +20,19 @@ public class EmployeeDoublyLinkedList {
         head = node;
         size++;
     }
+    public void addToEnd(Employee employee) {
+        EmployeeNode node = new EmployeeNode(employee);
+        if(tail == null) {
+            head = node;
+        }
+        else {
+            tail.setNext(node);
+            node.setPrevious(tail);
+        }
+
+        tail = node;
+        size++;
+    }
 
     public int getSize() {
         return size;
@@ -31,13 +44,40 @@ public class EmployeeDoublyLinkedList {
 
     public EmployeeNode removeFromFront() {
         if (isEmpty()) {
-
             return null;
         }
 
         EmployeeNode removedNode = head;
+
+        if(head.getNext() == null) {
+            tail = null;
+        }
+        else {
+            head.getNext().setPrevious(null);
+        }
+
         head = head.getNext();
         size--;
+        removedNode.setNext(null);
+        return removedNode;
+    }
+
+    public EmployeeNode removeFromEnd() {
+        if(isEmpty()) {
+            return null;
+        }
+
+        EmployeeNode removedNode = tail;
+
+        if(tail.getPrevious() == null) {
+            head = null;
+        }
+        else {
+            tail.getPrevious().setNext(null);
+        }
+        tail = tail.getPrevious();
+        size--;
+        removedNode.setPrevious(null);
         return removedNode;
     }
 
