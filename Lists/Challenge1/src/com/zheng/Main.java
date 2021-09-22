f481c09e04a67b2831f32493b37df7b2af1375fc
Wei <wzheng100@gmail.com>
2021-09-21 19:16:19 -0700

    Challenge

---
+++ Lists/Challenge1/src/com/zheng/Main.java
@@ -0,0 +1,25 @@
+package com.zheng;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+        Employee billEnd = new Employee("Bill", "End", 78);
+
+        EmployeeDoublyLinkedList list = new EmployeeDoublyLinkedList();
+
+        list.addToFront(janeJones);
+        list.addToFront(johnDoe);
+        list.addToFront(marySmith);
+        list.addToFront(mikeWilson);
+        list.printList();
+
+        list.addBefore(billEnd, johnDoe);
+        list.printList();
+
+        list.addBefore(new Employee("Someone", "Else", 111), mikeWilson);
+    }
+}
