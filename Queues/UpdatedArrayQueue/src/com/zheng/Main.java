f8680f65eb9ec7c7a637f62c8258677315c1d87f
Wei <wzheng100@gmail.com>
2021-09-29 15:54:43 -0700

    Optimizing

---
+++ Queues/UpdatedArrayQueue/src/com/zheng/Main.java
@@ -0,0 +1,43 @@
+package com.zheng;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+        Employee billEnd = new Employee("Bill", "End", 78);
+
+        ArrayQueue queue = new ArrayQueue(5);
+
+        queue.add(janeJones);
+        queue.add(johnDoe);
+        queue.remove();
+        queue.add(marySmith);
+        queue.remove();
+        queue.add(mikeWilson);
+        queue.remove();
+        queue.add(billEnd);
+        queue.remove();
+        queue.add(janeJones);
+
+        queue.printQueue();
+        System.out.println("================");
+
+        queue.remove();
+        queue.remove();
+        queue.printQueue();
+
+        System.out.println("================");
+        System.out.println(queue.peek());
+        queue.remove();
+        queue.remove();
+        queue.remove();
+
+        System.out.println("==========================");
+        queue.add(mikeWilson);
+
+        queue.printQueue();
+    }
+}
