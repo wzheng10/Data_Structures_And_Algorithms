ddeb961e7c752b7d380160f5e8aa7e9682d87b91
Wei <wzheng100@gmail.com>
2021-09-29 15:30:05 -0700

    Notes

--- Queues/Queues/src/com/zheng/ArrayQueue.java
+++ Queues/Queues/src/com/zheng/ArrayQueue.java
@@ -13,23 +13,25 @@ public class ArrayQueue {
     }
 
     public void add(Employee employee) {
-        if (back == queue.length) {
-            Employee[] newArray = new Employee[2 * queue.length];
-            System.arraycopy(queue, 0, newArray, 0, queue.length);
+
+        if (back == queue.length) {  //checking to see if it needs to be resized
+            Employee[] newArray = new Employee[2 * queue.length];   //doubles the size of the array
+            System.arraycopy(queue, 0, newArray, 0, queue.length);  //copies the elements into the resized array
             queue = newArray;
         }
 
-        queue[back] = employee;
-        back++;
+        queue[back] = employee; // adds the new employee onto the end of the line
+        back++; //increments back
     }
 
     public Employee remove() {
-        if (size() == 0) {
+        if (size() == 0) {  //checks if there are elements in the array
             throw new NoSuchElementException();
         }
-        Employee employee = queue[front];
+        Employee employee = queue[front]; //removes the employee, only at the front of the array
+        queue[front] = null; //clean up by replacing the removed element with a null placeholder
         front++;
-        if(size() == 0) {
+        if(size() == 0) {   //resetting the queue if the remove method removes the last element
             front = 0;
             back = 0;
         }
@@ -37,14 +39,14 @@ public class ArrayQueue {
         return employee;
     }
     public Employee peek() {
-        if(size() == 0) {
+        if(size() == 0) {   //cant peek if there is an empty queue
             throw new NoSuchElementException();
         }
 
-        return queue[front];
+        return queue[front];    //just returns the element in the first position
     }
 
-    public int size() {
+    public int size() { // based off of total size of array(back) and removed (front)
         return back - front;
     }
 
