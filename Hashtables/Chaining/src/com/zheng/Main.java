fa9e27fc0f34763875f008ce012c3f41712e0291
wzheng10 <wzheng100@gmail.com>
2021-11-20 17:54:20 -0800

    Rehashing

---
+++ Hashtables/Chaining/src/com/zheng/Main.java
@@ -0,0 +1,32 @@
+package com.zheng;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Employee janeJones = new Employee("Jane", "Jones", 123);
+	Employee johnDoe = new Employee ("John", "Doe", 4567);
+	Employee marySmith = new Employee("Mary", "Smith", 22);
+	Employee mikeWilson = new Employee("Mike", "Wilson", 3245 );
+	Employee billEnd = new Employee ("Bill", "End", 78);
+
+	SimpleHashtable ht = new SimpleHashtable();
+	ht.put("Jones", janeJones);
+	ht.put("Doe", johnDoe);
+	ht.put("Wilson", mikeWilson);
+	ht.put("Smith", marySmith);
+//	ht.put("End", billEnd);
+
+//	ht.printHashtable();
+
+	System.out.println("retrieve key Wilson: " + ht.get("Wilson"));
+	System.out.println("retrieve key Smith: " + ht.get("Smith") );
+
+	ht.remove("Wilson");
+	ht.remove("Jones");
+	ht.printHashtable();
+		System.out.println("==================================");
+
+	System.out.println("retrieve key Smith: " + ht.get("Smith") );
+	ht.printHashtable();
+    }
+}
